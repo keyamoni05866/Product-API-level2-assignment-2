7bd24f2688ffb857b98d1d1289e0fc40e56731af
keyamoni05866 <keya05866@gmail.com>
2024-05-22 12:50:50 +0600

    order data get done

--- src/app/modules/orders/order.model.ts
+++ src/app/modules/orders/order.model.ts
@@ -2,7 +2,7 @@ import { TOrder } from "./order.interface";
 import { Schema, model } from "mongoose";
 
 const orderSchema = new Schema<TOrder>({
-  email: { type: String, required: true, unique: true },
+  email: { type: String, required: true },
   productId: { type: String, required: true },
   price: { type: Number, required: true },
   quantity: { type: Number, required: true },
